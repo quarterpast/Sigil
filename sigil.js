b13026084c0d8a3a9719102e2fc07e95e0ef422c
Matt Brennan <mattyb1000@gmail.com>
2012-02-07 12:35:00 +0000

    add state.scope as an array of nested scopes

--- sigil.js
+++ sigil.js
@@ -15,25 +15,28 @@ var Sigil = {
 	compile: function(text) {
 		function blank(){return "";}
 		function Exit(){}
-		var state = {tag:"",scope:""},
+		var state = {tag:"",scope:[]},
 		sigils = {
 			"$": function variable_(expr) {
-				console.log(state.tag,expr)
 				return vm.createScript(expr);
 			},
 			"@": function list_(expr) {
+				state.scope.push(expr);
 				state.tag = "@";
 				return blank;
 			},
 			"%": function scope_(expr) {
+				state.scope.push(expr);
 				state.tag = "%";
 				return blank;
 			},
 			"?": function if_(expr) {
+				state.scope.push(expr);
 				state.tag = "?";
 				return blank;
 			},
 			"!": function else_(expr) {
+				state.scope.push(expr);
 				var close = '';
 				if(state.tag) {
 					this["/"](expr);
@@ -43,7 +46,9 @@ var Sigil = {
 				return blank;
 			},
 			"/": function end_(expr) {
-				console.log(state.tag)
+				console.log(state.tag,state.scope)
+				state.scope.pop();
+				state.tag = "";
 				return blank;
 			},
 			"<": function include_(expr) {
